3eda892c9c26102da1fa97e1e7278ddcf3eec79e
Susana <susanalozanoadrio@gmail.com>
2017-08-07 20:38:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var stick = {
+  width: 20,
+  height: 100
+};
+var yPositionStick = canvas.height/2 - stick.height/2;
+var xInitialPosition1 = 25;
+var xInitialPosition2 = canvas.width - xInitialPosition1 - stick.width;
+
+var xBall = canvas.width/2;
+var yBall = canvas.height/2;
+var yVelocity= 10;
+var xVelocity= 10;
+
+
+var radius = 15;
+
+function drawStick() {
+  if (canvas.getContext) { // si puedo acceder al elemento  comprueba si el método existe.
+    // tb comprueba si el navegador soporta canvas
+
+    ctx.fillStyle = 'orange'; // con esto se elige el color y con eso ya se puede pintar, es como elegir primro el pincel
+    ctx.fillRect(xInitialPosition1, yPositionStick, stick.width, stick.height);  // pinta en las cordenadas x y ancho y alto
+    // ctx.clearRect(45, 45, 60, 60); // borra
+    // ctx.strokeRect(50, 50, 50, 50); // pinta solo el borde
+    ctx.fillRect( xInitialPosition2, yPositionStick, stick.width, stick.height);
+  }
+}
+
+function drawBall(){
+  ctx.fillStyle = 'black';
+  ctx.beginPath();
+  ctx.arc(xBall, yBall, radius, 0, 2*Math.PI);
+  ctx.fill();
+}
+
+function draw(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawStick();
+  drawBall();
+}
+
+draw();
+
+setInterval(function (){
+  calculateBallPosition();
+  draw();
+}, 40);
+
+function calculateBallPosition(){
+  if (xBall + radius >= canvas.width || xBall - radius <= 0) {
+    xVelocity = -xVelocity;
+  }
+  if (yBall + radius >= canvas.height || yBall - radius <= 0) {
+    yVelocity = -yVelocity;
+  }
+  xBall = xBall + xVelocity;
+  yBall = yBall + yVelocity;
+}
